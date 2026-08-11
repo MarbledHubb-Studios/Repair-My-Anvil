c69d0057b5641de78f4eddfdd9d2290104f7df19
MarbledNull <cyandoezstuff@gmail.com>
2026-08-11 12:00:52 -0500

    initial

---
+++ src/main/java/com/marbledhubb/repair_my_anvil/RepairMyAnvil.java
@@ -0,0 +1,45 @@
+package com.marbledhubb.repair_my_anvil;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(RepairMyAnvil.MODID)
+public class RepairMyAnvil
+{
+    public static final String MODID = "repair_my_anvil";
+
+    public RepairMyAnvil(FMLJavaModLoadingContext context)
+    {
+        IEventBus modEventBus = context.getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
---
+++ src/main/java/com/marbledhubb/repair_my_anvil/event/AnvilRepairEvent.java
@@ -0,0 +1,67 @@
+package com.marbledhubb.repair_my_anvil.event;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import static net.minecraft.world.level.block.AnvilBlock.FACING;
+
+@Mod.EventBusSubscriber
+public class AnvilRepairEvent {
+
+    @SubscribeEvent
+    public static void onBlockInteract(PlayerInteractEvent.RightClickBlock event) {
+        Player player = event.getEntity();
+        Level level = event.getLevel();
+        BlockPos pos = event.getPos();
+        BlockState state = event.getLevel().getBlockState(pos);
+        InteractionHand hand = event.getHand();
+        ItemStack stack = player.getItemInHand(hand);
+        RandomSource source = RandomSource.createNewThreadLocalInstance();
+
+        double b0 = (double)pos.getX() + 0.0 + source.nextDouble();
+        double b1 = (double)pos.getY() + 1.0;
+        double b2 = (double)pos.getZ() + 0.0 + source.nextDouble();
+
+        if (stack.is(Items.IRON_INGOT)) {
+            if (state.is(Blocks.DAMAGED_ANVIL)) {
+                event.setCancellationResult(InteractionResult.SUCCESS);
+                event.setCanceled(true);
+                if (!player.isCreative()) {
+                    stack.shrink(1);
+                }
+                level.setBlock(pos, Blocks.CHIPPED_ANVIL.defaultBlockState().setValue(FACING, state.getValue(FACING)), 1);
+                level.playSound(player, pos, SoundEvents.ANVIL_PLACE, SoundSource.BLOCKS, 1.0f, 1.2F / (level.getRandom().nextFloat() + 0.2F));
+                level.addParticle(ParticleTypes.ELECTRIC_SPARK, b0, b1, b2, 0.1, 0.4, 0.1);
+                level.addParticle(ParticleTypes.ELECTRIC_SPARK, b0, b1, b2, -0.1, 0.4, -0.1);
+                player.awardStat(Stats.INTERACT_WITH_ANVIL);
+
+            } else if (state.is(Blocks.CHIPPED_ANVIL)) {
+                event.setCancellationResult(InteractionResult.SUCCESS);
+                event.setCanceled(true);
+                if (!player.isCreative()) {
+                    stack.shrink(1);
+                }
+                level.setBlock(pos, Blocks.ANVIL.defaultBlockState().setValue(FACING, state.getValue(FACING)), 1);
+                level.playSound(player, pos, SoundEvents.ANVIL_PLACE, SoundSource.BLOCKS, 1.0f, 1.2F / (level.getRandom().nextFloat() + 0.2F));
+                level.addParticle(ParticleTypes.ELECTRIC_SPARK, b0, b1, b2, 0.1, 0.4, 0.1);
+                level.addParticle(ParticleTypes.ELECTRIC_SPARK, b0, b1, b2, -0.1, 0.4, -0.1);
+                player.awardStat(Stats.INTERACT_WITH_ANVIL);
+            }
+        }
+    }
+}
